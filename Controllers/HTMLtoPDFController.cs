d189c9266e257ee0301dfcb37ce09aa6e4ae5096
Manuel Matos Guerrero <alexandermatosguerrero@hotmail.com>
2023-04-18 11:50:04 -0400

    first commit

---
+++ Controllers/HTMLtoPDFController.cs
@@ -0,0 +1,31 @@
+﻿using HTMLtoPDF.Models;
+using Microsoft.AspNetCore.Mvc;
+using Rotativa.AspNetCore;
+
+namespace HTMLtoPDF.Controllers
+{
+    public class HTMLtoPDFController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public Task<IActionResult> GenerarPDF()
+        {
+            List<Carros> carro = new List<Carros>()
+            {
+                new Carros(){ Placa = "Q3W4E5", Color = "Rojo", Modelo = "Hilux", Marca = "Toyota", Tipo = "Carga"},
+                new Carros(){ Placa = "Q4W5E6", Color = "Verde", Modelo = "Hilux", Marca = "Toyota", Tipo = "Carga"},
+                new Carros(){ Placa = "Q6W7E8", Color = "Azul", Modelo = "Hilux", Marca = "Toyota", Tipo = "Carga" }
+            };
+
+            return Task.FromResult<IActionResult>(new ViewAsPdf("GenerarPDF", carro)
+            {
+                FileName = $"HTMLtoPDF.pdf",
+                PageOrientation = Rotativa.AspNetCore.Options.Orientation.Portrait,
+                PageSize = Rotativa.AspNetCore.Options.Size.A4
+            });
+        }
+    }
+}
---
+++ Models/Carros.cs
@@ -0,0 +1,11 @@
+﻿namespace HTMLtoPDF.Models
+{
+    public class Carros
+    {
+        public string Placa { get; set; }
+        public string Color { get; set;}
+        public string Marca { get; set;}
+        public string Modelo { get; set;}
+        public string Tipo { get; set;}
+    }
+}
